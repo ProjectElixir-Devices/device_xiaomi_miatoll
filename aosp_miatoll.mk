f73edc8bd02b2377013ceccdb708a081055c3126
Arindam Bhattacharjee <abhattacharjee717@gmail.com>
2024-03-27 23:28:35 +0530

    miatoll: cleanup!
    Signed-off-by: Arindam Bhattacharjee <abhattacharjee717@gmail.com>
    
    Revert "miatoll: Removed some packages"
    
    This reverts commit 94f30a41e93c423b0b52af0d8d7750f19e28cc8a.

--- aosp_miatoll.mk
+++ aosp_miatoll.mk
@@ -7,9 +7,6 @@
 $(call inherit-product, device/xiaomi/miatoll/device.mk)
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-# LMC-Camera
-# $(call inherit-product, vendor/xiaomi/LMC-Camera/config.mk)
-
 PRODUCT_NAME := aosp_miatoll
 PRODUCT_DEVICE := miatoll
 PRODUCT_BRAND := Xiaomi
@@ -22,16 +19,15 @@ WITH_GMS := true
 IS_PHONE := true
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
-TARGET_INCLUDE_STOCK_ACORE := false
-TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_INCLUDE_STOCK_ACORE := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_SUPPORTS_QUICK_TAP := true
 
-# Elixir
 ELIXIR_BUILD_TYPE := OFFICIAL
 EXCLUDE_LAWNCHAIR := true
 BUILD_USERNAME := userariii
-BUILD_HOSTNAME := Elixir
+BUILD_HOSTNAME := aosp
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="miatoll_global-user 12 RKQ1.211019.001 V13.0.2.0.SJWMIXM release-keys"
